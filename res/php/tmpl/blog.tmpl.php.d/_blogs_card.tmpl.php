5ffecad9219b4fd8c2141bb7528f15bf44ffcb30
xet-labs <rishikeshprasad8512@gmail.com>
2024-08-25 00:49:49 +0530

    blogs-card layout complete

--- res/php/tmpl/blog.tmpl.php.d/_blogs_card.tmpl.php
+++ res/php/tmpl/blog.tmpl.php.d/_blogs_card.tmpl.php
@@ -16,51 +16,67 @@
         </a>
     </div>
     
-    <div class="post-info">
-
-        <a href="<?php echo htmlspecialchars($Blog['path']); ?>" class="post-title">
-            <?php echo htmlspecialchars($Blog['short_title']??$Blog['title']); ?>
-        </a>
-
-        <p class="post-excerpt">
-            <?php echo htmlspecialchars($Blog['excerpt']); ?>
-        </p>
-
-        
-        <!-- <div class="post-meta-wrap"> -->
-        <div class="post-meta">
-
-            <div class="meta-author-img">
-                <a href="">
-                    <img src="<?php echo htmlspecialchars($Blog['profile_img']); ?>" alt="<?php echo htmlspecialchars($Blog['author']); ?>" loading="lazy" fetchpriority="low">
-                </a>
-            </div>
-
-            <div class="meta-info">
-
-                <div class="meta-author">
-                    <span>
-                        <a href="">
-                            <?php echo htmlspecialchars($Blog['name'] . " ". $Blog['name_l']); ?>
-                            <!-- <?php echo htmlspecialchars($Blog['uid']); ?> -->
-                        </a>
-                    </span>
-                </div>
-
-                <div class="meta-date">
-                    <span>
-                        <?php
-                            // echo htmlspecialchars(date('F j, Y, g:i A', strtotime($Blog['created_at'])));
-                            util::getTimeAgo($Blog['created_at']);
-                        ?>
-                    </span>
-                </div>
-            
-            </div>
-
-        </div>
-        <!-- </div> -->
+    <div class="post-info-wrap">
+        <!-- <div class="meta-author-img">
+            <a href="">
+                <img src="<?php echo htmlspecialchars($Blog['profile_img']); ?>" alt="<?php echo htmlspecialchars($Blog['author']); ?>" loading="lazy" fetchpriority="low">
+            </a>
+        </div> -->
     
+        <div class="post-info">
+
+            <a href="<?php echo htmlspecialchars($Blog['path']); ?>" class="post-title">
+                <?php echo htmlspecialchars($Blog['short_title']??$Blog['title']); ?>
+            </a>
+
+            <p class="post-excerpt">
+                <?php echo htmlspecialchars($Blog['excerpt']); ?>
+            </p>
+
+            
+            <!-- <div class="post-meta-wrap"> -->
+            <div class="post-meta">
+
+                <div class="meta-author-img">
+                    <a href="">
+                        <img src="<?php echo htmlspecialchars($Blog['profile_img']); ?>" alt="<?php echo htmlspecialchars($Blog['author']); ?>" loading="lazy" fetchpriority="low">
+                    </a>
+                </div>
+
+                <div class="meta-info">
+
+                    <div class="meta-author">
+                        <span>
+                            <a href="/profile/<?php echo htmlspecialchars($Blog['uid']); ?>">
+                                <?php echo htmlspecialchars($Blog['name'] . " ". $Blog['name_l']); ?>
+                            </a>
+                        </span>
+
+                        <?php if($Blog['verified']):?>
+                            <div class="meta-author-isverified">
+                                <?php echo htmlspecialchars($Blog['uid']); ?>
+
+                            </div>
+                        <?php endif; ?>
+
+
+                    </div>
+
+                    <div class="meta-date">
+                        <span>
+                            <?php
+                                // echo htmlspecialchars(date('F j, Y, g:i A', strtotime($Blog['created_at'])));
+                                util::getTimeAgo($Blog['created_at']);
+                            ?>
+                        </span>
+                    </div>
+                
+                </div>
+
+            </div>
+            <!-- </div> -->
+        
+        </div>
     </div>
 
 </div>
