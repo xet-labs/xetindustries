3e688f4c041edae62eed5f2241649653be403e48
root <root@xlvini>
2025-01-13 02:31:20 +0530

    stable-bkp Mon Jan 13 02:31:20 IST 2025

--- asset/res/partial/00-head/meta.head.prtl.php
+++ asset/res/partial/00-head/meta.head.prtl.php
@@ -1,45 +1,44 @@
 <?php
-
+$PAGE->appName = empty($PAGE->appName) ? htmlspecialchars(config('app.name')) : htmlspecialchars($PAGE->appName) ; 
 $PAGE->author = empty($PAGE->author) && !empty($PAGE->username) ? htmlspecialchars($PAGE->name . ' ' . $PAGE->name_l) : htmlspecialchars($PAGE->author);
-$PAGE->authorUrl = empty($PAGE->authorUrl) && !empty($PAGE->username) ? '/@' . htmlspecialchars($PAGE->username) : htmlspecialchars($PAGE->authorUrl);
+$PAGE->authorUrl = empty($PAGE->authorUrl) && !empty($PAGE->username) ? url('/') . '/@' . htmlspecialchars($PAGE->username) : htmlspecialchars($PAGE->authorUrl);
 $PAGE->baseUrl = empty($PAGE->baseUrl) ? htmlspecialchars(url('/') . $_SERVER['REQUEST_URI'], ENT_QUOTES, 'UTF-8') : '';
 $PAGE->metaTitle = (
     !empty($PAGE->title) ?
         htmlspecialchars($PAGE->title) .
         (!empty($PAGE->username) ? ' | by ' . htmlspecialchars($PAGE->author) : '') .
-        ' | ' . e(config('app.name'))
-    : e(config('app.name'))
+        ' | ' . $PAGE->appName
+    : $PAGE->appName
 );
 ?>
 
 <title><?= $PAGE->metaTitle ?></title>
 
-<?= '<meta name="title" content="' . $PAGE->metaTitle . '" />' ?>
 <?= empty($PAGE->canonical)? '<link rel="canonical" href="' . htmlspecialchars($PAGE->baseUrl) . '" />' : '' ?>
+<?= '<meta name="title" content="' . $PAGE->metaTitle . '" />' ?>
 <?= !empty($PAGE->excerpt) ? '<meta name="description" content="' . htmlspecialchars($PAGE->excerpt) . '" />' : ''; ?>
-
-<meta name="robots" content="index,noarchive,follow" />
 
 <?php if ($PAGE->type === 'article'): ?>
     <?= !empty($PAGE->username) ? '<meta name="author" content="' .  htmlspecialchars($PAGE->author) . '" />' : ''; ?>
-    <?= !empty($PAGE->authorUrl) ? '<link rel="author" href="' . htmlspecialchars(url('/') . $PAGE->authorUrl) . '" />' : ''; ?>
+    <?= !empty($PAGE->authorUrl) ? '<link rel="author" href="' . htmlspecialchars($PAGE->authorUrl) . '" />' : ''; ?>
     <?= !empty($PAGE->created_at) ? '<meta property="article:published_time" content="' . htmlspecialchars($PAGE->created_at) . '" />' : ''; ?>
 <?php endif; ?>
 
-<?= !empty($PAGE->generator) ? '<meta name="generator" content="' . htmlspecialchars($PAGE->generator) . '" />' : ''; ?>
+<meta name="robots" content="index,noarchive,follow" />
 <?= !empty($PAGE->keywords) ? '<meta name="keywords" content="Xet, Xet Industries, XetIndustries, Xtreme Embeded Tech Industries, ' . htmlspecialchars($PAGE->keywords) . '" />' : ''; ?>
 
 <!-- Open Graph -->
 <?= empty($PAGE->type) ? '<meta property="og:type" content="website" />' : '<meta property="og:type" content="' . $PAGE->type . '" />'; ?>
-<?= !empty($PAGE->title) ? '<meta property="og:title" content="' . htmlspecialchars($PAGE->title) . '" />' : ''; ?>
 <?= empty($PAGE->canonical)? '<meta property="og:url" content="' . $PAGE->baseUrl . '" />' : ''; ?>
+<?= !empty($PAGE->title) ? '<meta property="og:title" content="' . htmlspecialchars($PAGE->title) . '" />' : ''; ?>
 <?= !empty($PAGE->excerpt) ? '<meta property="og:description" content="' . htmlspecialchars($PAGE->excerpt) . '" />' : ''; ?>
 <?= !empty($PAGE->featured_img) ? '<meta property="og:image:secure_url" content="' . htmlspecialchars($PAGE->featured_img) . '" />' : ''; ?>
+<?= '<meta property="og:site_name" content="' . htmlspecialchars($PAGE->appName) . '" />' ; ?>
 
 
 <!-- Twitter -->
-<?= !empty($PAGE->title) ? '<meta name="twitter:title" content="' . htmlspecialchars($PAGE->title) . '">' : ''; ?>
 <?= empty($PAGE->canonical) ? '<meta name="twitter:url" content="' . htmlspecialchars($PAGE->baseUrl) . '">' : ''; ?>
+<?= !empty($PAGE->title) ? '<meta name="twitter:title" content="' . htmlspecialchars($PAGE->title) . '">' : ''; ?>
 <?= !empty($PAGE->excerpt) ? '<meta name="twitter:description" content="' . htmlspecialchars($PAGE->excerpt) . '">' : ''; ?>
 <?= !empty($PAGE->featured_img) ? '<meta name="twitter:image" content="' . htmlspecialchars($PAGE->featured_img) . '">' : ''; ?>
 <?= !empty($PAGE->site_domain) ? '<meta name="twitter:domain" value="' . htmlspecialchars($PAGE->site_domain) . '" />' : ''; ?>
